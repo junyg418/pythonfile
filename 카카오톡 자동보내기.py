ae3600f121fa7898044363bc46811b860d6eac86
이준영 <91041950+junyg418@users.noreply.github.com>
2022-07-10 14:18:25 +0900

    카카오톡 자동보내기 (초기 자료)

--- 카카오톡 자동보내기.py
+++ 카카오톡 자동보내기.py
@@ -1,3 +1,11 @@
+'''
+설명 주석
+    구글에서 가져왔었던 파이썬 자동보내기 코드
+    이를 분석하고 zoomdon't 프로젝트에 추가하려 코드를 작성했음
+    초기자료
+    Update and rename test_2.py to 카카오톡 자동보내기.py
+'''
+
 from typing import Text
 import win32api, win32con, win32gui, time
 
